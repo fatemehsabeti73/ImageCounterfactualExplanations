36bd076fb2131073740032fd3f1db1decd6c9474
fatemehsabeti73 <fatemehsabeti73@gmail.com>
2025-08-10 18:26:20 +0330

    Update explain_instance_shap.py

--- isedc/explain_instance_shap.py
+++ isedc/explain_instance_shap.py
@@ -12,34 +12,34 @@ def explain_instance_shap(image, classifier, segments, number_features):
     # define a function that depends on a binary mask representing if an image region is hidden
     def mask_image(zs, segmentation, image, background=None):
         if background is None:
-            background = image.mean((0,1))
+            background = image.mean((0, 1))
         out = np.zeros((zs.shape[0], image.shape[0], image.shape[1], image.shape[2]))
         for i in range(zs.shape[0]):
-            out[i,:,:,:] = image
+            out[i, :, :, :] = image
             for j in range(zs.shape[1]):
-                if zs[i,j] == 0:
-                    out[i][segmentation == j,:] = background
+                if zs[i, j] == 0:
+                    out[i][segmentation == j, :] = background
         return out
+
     def f(z):
         return classifier.predict(mask_image(z, segments, image))
     
     # use Kernel SHAP to explain the network's predictions
     n_segments = len(np.unique(segments))
-    explainer = shap.KernelExplainer(f, np.zeros((1,n_segments)))
-    shap_values = explainer.shap_values(np.ones((1,n_segments)), nsamples=1000)  
+    explainer = shap.KernelExplainer(f, np.zeros((1, n_segments)))
+    shap_values = explainer.shap_values(np.ones((1, n_segments)), nsamples=1000)
     
     pred_class = np.argmax(classifier.predict(image[np.newaxis, ...])[0])
-if len(shap_values) == 1:
-    relevant_shap = shap_values[0]  # اگر shap فقط برای یک کلاس باشد
-else:
-    relevant_shap = shap_values[pred_class]
+    if len(shap_values) == 1:
+        relevant_shap = shap_values[0]  # اگر shap فقط برای یک کلاس باشد
+    else:
+        relevant_shap = shap_values[pred_class]
+    
     ranked_segments = np.argsort(-relevant_shap)
 
-    explanation = np.full([image.shape[0],image.shape[1],image.shape[2]],0/255.0)
-    for i in ranked_segments[0,0:number_features]:
+    explanation = np.full([image.shape[0], image.shape[1], image.shape[2]], 0/255.0)
+    for i in ranked_segments[0, 0:number_features]:
         explanation[segments == i] = image[segments == i]
         
-    segments_in_explanation = ranked_segments[0,0:number_features]
+    segments_in_explanation = ranked_segments[0, 0:number_features]
     return explanation, segments_in_explanation
-
-
