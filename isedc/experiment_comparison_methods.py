4c30f9acac65dbc388e9a836668a8532dd076d90
fatemehsabeti73 <fatemehsabeti73@gmail.com>
2025-08-10 17:24:35 +0330

    Update experiment_comparison_methods.py

--- isedc/experiment_comparison_methods.py
+++ isedc/experiment_comparison_methods.py
@@ -5,39 +5,44 @@ Created on August 10, 2025
 @author: FSM
 """
 
-import time
-import matplotlib.pylab as plt
-import numpy as np
-import PIL.Image as Image
 import os
-from os import listdir
+import time
+import numpy as np
 import pandas as pd
+import matplotlib.pylab as plt
+from os import listdir
 import tensorflow as tf
 import tensorflow_hub as hub
-from skimage.segmentation import quickshift
+import PIL.Image as Image
 import cv2
+from skimage.segmentation import quickshift
 
-# Local methods
+# Clone the repository to /content
+if not os.path.exists('/content/ImageCounterfactualExplanations'):
+    !git clone https://github.com/fatemehsabeti73/ImageCounterfactualExplanations.git
+
+# Import local scripts
+os.chdir('/content/ImageCounterfactualExplanations')
 from sedc_time import sedc_time
 from explain_instance_lime import explain_instance_lime
 from explain_instance_shap import explain_instance_shap
 from perform_occlusion_analysis import perform_occlusion_analysis
 
 # Model import
-classifier_url ="https://tfhub.dev/google/tf2-preview/mobilenet_v2/classification/2"
+classifier_url = "https://tfhub.dev/google/tf2-preview/mobilenet_v2/classification/2"
 IMAGE_SHAPE = (224, 224)
-
 classifier = tf.keras.Sequential([
     hub.KerasLayer(classifier_url, input_shape=IMAGE_SHAPE+(3,))
 ])
 
+# Load ImageNet labels
 labels_path = tf.keras.utils.get_file(
     'ImageNetLabels.txt',
     'https://storage.googleapis.com/download.tensorflow.org/data/ImageNetLabels.txt'
 )
 imagenet_labels = np.array(open(labels_path).read().splitlines())
 
-# Image loader
+# Image loading function
 def loadImages(path, imshape):
     imagesList = listdir(path)
     loadedImages = []
@@ -48,10 +53,9 @@ def loadImages(path, imshape):
             loadedImages.append(img)
     return loadedImages
 
-# Base paths
-base_dir = os.getcwd()
-path = os.path.join(base_dir, "images")  # assumes 'images/class_name/' exists
-classes = listdir(path)
+# Set path to the test images
+path_images_root = '/content/ImageCounterfactualExplanations/chihuahua_test'
+classes = ['chihuahua_test']  # only one class here
 
 # Similarity function
 def calculate_similarity(segments_explanations):
@@ -64,40 +68,45 @@ def calculate_similarity(segments_explanations):
                 counter += 1
         if counter == len(segments_explanations):
             intersection.append(i)
-    return len(intersection) / len(union)
+    similarity = len(intersection) / len(union)
+    return similarity
 
-# Experiment setup
+# Experiment parameters
 table = pd.DataFrame(columns=[
-    'Image', 'k_SEDC', 'similarity_sedc', 'similarity_lime',
-    'similarity_shap', 'similarity_occlusion',
-    'mean_ct_sedc', 'mean_ct_lime', 'mean_ct_shap', 'mean_ct_occlusion',
-    'times_counterfactual_lime', 'times_counterfactual_shap',
+    'Image', 'k_SEDC', 'similarity_sedc', 'similarity_lime', 'similarity_shap', 
+    'similarity_occlusion', 'mean_ct_sedc', 'mean_ct_lime', 'mean_ct_shap', 
+    'mean_ct_occlusion', 'times_counterfactual_lime', 'times_counterfactual_shap', 
     'times_counterfactual_occlusion'
-])
+], index=[i for i in range(200)])
 
-n_runs = 10
-time_limit = 60
-images_per_class = 10
 n = 0
+n_runs = 2   # reduce for quick test
+time_limit = 60
+images_per_class = 5  # limit for speedy testing
 
 for class_name in classes:
-    path_images = os.path.join(path, class_name)
+    path_images = path_images_root
     images = loadImages(path_images, IMAGE_SHAPE)
     print('Class ' + class_name + ' started')
-    images = images[0:images_per_class*2]
-    counter = 0
 
+    images = images[:images_per_class]
+
+    counter = 0
     for image in images:
+        # Classification
         result = classifier.predict(image[np.newaxis, ...])
         predicted_class = np.argmax(result[0], axis=-1)
-        segments = quickshift(image, kernel_size=4, max_dist=200, ratio=0.2)
-        perturbed_image = cv2.GaussianBlur(image, (31, 31), 0)
+        print(f"Prediction: {imagenet_labels[predicted_class]}")
 
-        # SEDC
+        # Segmentation
+        segments = quickshift(image, kernel_size=4, max_dist=200, ratio=0.2)
+        perturbed_image = cv2.GaussianBlur(image, (31,31), 0)
+
+        # ----- SEDC -----
         segments_in_sedc_explanations = []
         computation_times_sedc = []
         too_long = False
-        for _ in range(n_runs):
+        for i in range(n_runs):
             start = time.time()
             explanation, segments_in_explanation, perturbation, new_class, too_long = sedc_time(
                 image, classifier, segments, 'blur', time_limit
@@ -105,63 +114,63 @@ for class_name in classes:
             stop = time.time()
             if too_long:
                 break
-            segments_in_sedc_explanations.append(segments_in_explanation)
-            computation_times_sedc.append(stop - start)
+            else:
+                n_segments = len(segments_in_explanation)
+                segments_in_sedc_explanations.append(segments_in_explanation)
+                computation_times_sedc.append(stop-start)
         if too_long:
             continue
 
-        # LIME
+        # ----- LIME -----
         segments_in_lime_explanations = []
         computation_times_lime = []
         counter_lime = 0
-        n_segments = len(segments_in_explanation)
-        for _ in range(n_runs):
+        for i in range(n_runs):
             start = time.time()
-            _, mask_lime = explain_instance_lime(image, classifier, n_segments)
+            explanation_lime, mask_lime = explain_instance_lime(image, classifier, n_segments)
             stop = time.time()
             segments_in_lime_explanations.append(np.unique(segments[mask_lime == 1]))
-            computation_times_lime.append(stop - start)
+            computation_times_lime.append(stop-start)
+            # counterfactual test
             test_image = image.copy()
             for j in np.unique(segments[mask_lime == 1]):
                 test_image[segments == j] = perturbed_image[segments == j]
             if np.argmax(classifier.predict(test_image[np.newaxis, ...])) != predicted_class:
                 counter_lime += 1
-        if len(np.unique([len(i) for i in segments_in_lime_explanations])) > 1:
-            continue
 
-        # SHAP
+        # ----- SHAP -----
         segments_in_shap_explanations = []
         computation_times_shap = []
         counter_shap = 0
-        for _ in range(n_runs):
+        for i in range(n_runs):
             start = time.time()
-            _, segs_shap = explain_instance_shap(image, classifier, segments, n_segments)
+            explanation_shap, segs_shap = explain_instance_shap(image, classifier, segments, n_segments)
             stop = time.time()
             segments_in_shap_explanations.append(segs_shap)
-            computation_times_shap.append(stop - start)
+            computation_times_shap.append(stop-start)
             test_image = image.copy()
             for j in segs_shap:
                 test_image[segments == j] = perturbed_image[segments == j]
-            if np.argmax(classifier.predict(test_image[np.newaxis, ...])) != predicted_class:
+            if np.argmax(classifier.predict(test_image[np.newaxis,...])) != predicted_class:
                 counter_shap += 1
 
-        # Occlusion
+        # ----- Occlusion -----
         segments_in_occlusion_explanations = []
         computation_times_occlusion = []
         counter_occlusion = 0
-        for _ in range(n_runs):
+        for i in range(n_runs):
             start = time.time()
-            _, segs_occ = perform_occlusion_analysis(image, classifier, segments, n_segments)
+            explanation_occlusion, segs_occ = perform_occlusion_analysis(image, classifier, segments, n_segments)
             stop = time.time()
             segments_in_occlusion_explanations.append(segs_occ)
-            computation_times_occlusion.append(stop - start)
+            computation_times_occlusion.append(stop-start)
             test_image = image.copy()
             for j in segs_occ:
                 test_image[segments == j] = perturbed_image[segments == j]
             if np.argmax(classifier.predict(test_image[np.newaxis, ...])) != predicted_class:
                 counter_occlusion += 1
 
-        # Metrics
+        # ----- Save metrics -----
         table.loc[n] = [
             image, n_segments,
             calculate_similarity(segments_in_sedc_explanations),
@@ -174,14 +183,13 @@ for class_name in classes:
             np.mean(computation_times_occlusion),
             counter_lime, counter_shap, counter_occlusion
         ]
-        n += 1
         counter += 1
+        n += 1
         print(f"Iteration {n} done")
 
-        # Save incrementally
-        table.to_excel(os.path.join(base_dir, 'table.xlsx'))
-        if counter == images_per_class:
-            break
+# Save table
+table.to_excel('/content/results_table.xlsx')
+print("Results saved to /content/results_table.xlsx")
 
 
 
